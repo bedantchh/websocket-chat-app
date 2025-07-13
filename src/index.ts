7d468a6778c79345076ebb1f3220e0f135c3c7c2
Bedant Chhetri <bedantchhetri7@gmail.com>
2025-07-14 00:23:04 +0530

    Add WebSocket server implementation and update package.json scripts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 import { WebSocketServer } from "ws";
 
 const wss= new WebSocketServer({ port: 8080 });
+
+let userCount = 0;
+
+wss.on("connection",(socket)=>{
+    userCount++;
+    console.log(`current user count: ${userCount}`)
+    socket.send("Welcome to WebSocketServer!")
+    socket.on("message",(data)=>{
+        socket.send("Hello!")
+    })
+})
