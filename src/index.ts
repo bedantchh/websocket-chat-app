83d36ba48e13ed129eaff261b2b853345bb8650f
Bedant Chhetri <bedantchhetri7@gmail.com>
2025-07-16 23:26:21 +0530

    Refactor WebSocket server to manage user connections and messaging

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,40 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss= new WebSocketServer({ port: 8080 });
 
+interface User{
+        socket: WebSocket;
+        room: string;
+}
+
 let userCount = 0;
+let allSockets: User[] = [];
 
 wss.on("connection",(socket)=>{
-    userCount++;
-    console.log(`current user count: ${userCount}`)
-    socket.send("Welcome to WebSocketServer!")
-    socket.on("message",(data)=>{
-        socket.send("Hello!")
+    socket.on("message",(message)=>{
+        const parsedMessage = JSON.parse(message.toString())
+        console.log(parsedMessage)
+        if(parsedMessage.type === "join"){
+            console.log("user wants to join")
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload?.roomId
+            })
+        }
+        if(parsedMessage.type === "chat"){
+            console.log("user wants to message")
+            let currentUserRoom = null;
+            for(let i=0;i<allSockets.length;i++){
+                if(allSockets[i].socket === socket){
+                    currentUserRoom = allSockets[i].room
+                }
+            }
+
+            for(let i=0;i<allSockets.length;i++){
+                if(allSockets[i].room === currentUserRoom){
+                    allSockets[i].socket.send(parsedMessage.payload.message)
+                }
+            }
+        }
     })
 })
